64176384362e4956d3a1c8c47c61687c57394170
Bruno Cascio <brunocascio@gmail.com>
2016-03-05 18:28:29 -0300

    Role middleware

---
+++ app/Http/Middleware/RoleMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class RoleMiddleware
+{
+  /**
+  * Run the request filter.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  \Closure  $next
+  * @param  string  $role
+  * @return mixed
+  */
+  public function handle($request, Closure $next, $role)
+  {
+
+    if (! $request->user()->is($role)) {
+      return response('Unauthorized.', 401);
+    }
+
+    return $next($request);
+  }
+}
