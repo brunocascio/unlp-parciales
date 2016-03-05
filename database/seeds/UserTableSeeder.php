01932f1043a53afeeaff8f42600ea49aa6bcfe74
Bruno Cascio <brunocascio@gmail.com>
2016-03-05 18:29:35 -0300

    Seeder for admin user

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\UnxUser as User;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+          'name' => 'admin',
+          'email' => 'admin@site.com',
+          'password' => bcrypt('change_this_password'),
+        ]);
+    }
+}
