97f4bd99b51efe7a6269f8182ae2b26f625f0a70
Bruno Cascio <brunocascio@gmail.com>
2016-03-12 00:21:43 -0300

    fix filename saved into db

--- app/Http/Controllers/CourseResourceController.php
+++ app/Http/Controllers/CourseResourceController.php
@@ -8,84 +8,24 @@ use App\Http\Requests;
 
 use App\Course;
 
-class CourseResourceController extends AdminController
+class CourseResourceController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @param  String  $course_slug
-     * @return \Illuminate\Http\Response
-     */
-    public function index($course_slug, $type = null)
-    {
-      $course = Course::where('slug', $course_slug)->firstOrFail();
+  /**
+  * Display a listing of the resource.
+  *
+  * @param  String  $course_slug
+  * @param  String  $type_slug
+  * @return \Illuminate\Http\Response
+  */
+  public function index($course_slug, $type_slug = null)
+  {
 
-      dd($course->toJson());
-    }
+    $res = Course::where('slug', $course_slug)
+      ->firstOrFail()
+      ->resources();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+    if ($type_slug) $res->withTypeSlug($type_slug);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    return view('resources.index', ["resources" => $res->get()]);
+  }
 }
