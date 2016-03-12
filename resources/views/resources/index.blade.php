5c68154bdbb6ad6d17a0d6a5f101a2ed5eff83aa
Bruno Cascio <brunocascio@gmail.com>
2016-03-12 00:22:49 -0300

    resources list

--- resources/views/resources/index.blade.php
+++ resources/views/resources/index.blade.php
@@ -1,7 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container-fluid">
-    hola
-  </div>
+  @foreach($resources as $resource)
+    <article class="col-md-3">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h2 class="panel-title">{{ $resource->name }}</h2>
+        </div>
+        <div class="panel-body">
+          <div class="">
+            <strong>Profesor:</strong>
+            <span>{{ $resource->teacher->name }}</span>
+          </div>
+          @if ( $resource->number )
+            <div class="">
+              <strong>Fecha/Mesa:</strong>
+              <span>{{ $resource->number }}</span>
+            </div>
+          @endif
+        </div>
+        <div class="panel-footer">
+          @foreach($resource->files as $file)
+            <a href="{{ get_public_files_url($file->url) }}">Descargar</a>
+          @endforeach
+        </div>
+      </div>
+    </article>
+  @endforeach
 @endsection
