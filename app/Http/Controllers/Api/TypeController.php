dfefe3109b61084c4cc2eededfcd131ca231a8fb
Bruno Cascio <brunocascio@gmail.com>
2016-03-12 00:21:15 -0300

    Api Types endpoint

--- app/Http/Controllers/Api/TypeController.php
+++ app/Http/Controllers/Api/TypeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+
+use DB;
 use App\Career;
 
 class TypeController extends Controller
@@ -14,11 +16,18 @@ class TypeController extends Controller
   */
   public function typesOfCourse($career_slug, $course_slug)
   {
-    $career = Career::where('slug', $career_slug)->firstOrFail();
+    $types = DB::table('types')
+      ->join('resources', 'resources.type_id', '=', 'types.id')
+      ->join('courses', 'resources.course_id', '=', 'courses.id')
+      ->join('careers_courses', 'careers_courses.course_id', '=', 'courses.id')
+      ->join('careers', 'careers_courses.career_id', '=', 'careers.id')
+      ->where('courses.slug', $course_slug)
+      ->where('careers.slug', $career_slug)
+      ->select('types.*')
+      ->distinct()
+      ->get();
 
-    $course = $career->courses->where('slug', $course_slug)->first();
-
-    return response()->json(['types' => $course->types()]);
+    return response()->json(['types' => $types]);
 
   }
 
