7e4efbaa3e9019f1a4973dcd61b7ea274377af8a
Bruno Cascio <brunocascio@gmail.com>
2016-03-13 16:51:31 -0300

    Changes admin password on seed (uses random string instead)

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -5,18 +5,20 @@ use App\UnxUser as User;
 
 class UserTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        User::create([
-          'name' => 'admin',
-          'email' => 'admin@site.com',
-          'password' => bcrypt('change_this_password'),
-          'role' => 'admin'
-        ]);
-    }
+  /**
+  * Run the database seeds.
+  *
+  * @return void
+  */
+  public function run()
+  {
+    $password = str_random(10);
+
+    User::create([
+      'name' => 'admin',
+      'email' => 'admin@site.com',
+      'password' => bcrypt($password),
+      'role' => 'admin'
+    ]);
+  }
 }
