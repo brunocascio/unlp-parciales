68473ad744525d19ebb3cdb012b0b47340a80b24
Bruno Cascio <brunocascio@gmail.com>
2016-03-19 23:50:28 -0300

    Added Dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,9 @@
 @extends('admin.template')
+
+@section('main')
+  <aside class="jumbotron">
+    <div class="text-center">
+      <h1>Hi {{ Auth::user()->name }}!</h1>
+    </div>
+  </aside>
+@endsection
