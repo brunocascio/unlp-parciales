a68cabcd5c3989575c0895a9f076276a4c6c81d2
Bruno Cascio <brunocascio@gmail.com>
2016-08-08 19:38:40 -0300

    Improved UX

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,68 +1,47 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-  <title>@yield('title', trans('texts.home')) | {{ getSiteTitle() }}</title>
-  <!-- Fonts -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
-  <!-- Styles -->
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-  <link href="https://bootswatch.com/sandstone/bootstrap.min.css" rel="stylesheet">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/css/bootstrap-select.min.css" rel="stylesheet">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.3.1/css/fileinput.min.css" rel="stylesheet">
-  @section('stylesheets_default')
-    <link href="{{ asset('css/loader.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
-  @show
-  @stack('styles')
-</head>
-<body>
-  @include('partials.menu')
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>@yield('title', trans('texts.home')) | {{ getSiteTitle() }}</title>
+    <!-- Fonts -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    <!-- Styles -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://bootswatch.com/sandstone/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/css/bootstrap-select.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.3.1/css/fileinput.min.css" rel="stylesheet">
+    @section('stylesheets_default')
+      <link href="{{ asset('css/loader.css') }}" rel="stylesheet">
+      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+      <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
+    @show
+    @stack('styles')
+  </head>
+  <body>
+    @include('partials.menu')
 
-  @include('partials.notifications')
+    @include('partials.notifications')
 
-  <section class="container-fluid">
-    @yield('sidebar')
-    @yield('content')
-  </section>
+    <section class="container-fluid">
+      @yield('sidebar')
+      @yield('content')
+    </section>
 
-  @section('analitycs')
-    @if ( has_google_analitycs() )
+    @include('partials.analytics')
+
+    @section('javascripts_default')
+      <!-- JavaScripts -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/js/bootstrap-select.min.js"></script>
       <script type="text/javascript">
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-          ga('create', '{{ google_analitycs_code() }}', 'auto');
-          ga('send', 'pageview');
+        $('.selectpicker').attr('title', "{{trans('texts.select')}}");
       </script>
-    @endif
-  @show
+      <script src="{{ asset('js/app.js') }}"></script>
+    @show
 
-  <!-- JavaScripts -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.10.0/js/bootstrap-select.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.3.1/js/fileinput.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.3.1/js/fileinput_locale_es.min.js"></script>
-  @section('javascripts_default')
-    <script type="text/javascript">
-      $('.selectpicker').attr('title', "{{trans('texts.select')}}");
-      $('[type="file"]').fileinput({
-        showUpload: false,
-        overwriteInitial: false,
-        maxFileSize: 10000,
-        minFileCount: 1,
-        maxFileCount: 1,
-        language: "{{ App::getLocale() }}"
-      });
-    </script>
-    <script src="{{ asset('js/app.js') }}"></script>
-  @show
-  @stack('scripts')
-</body>
+    @stack('scripts')
+  </body>
 </html>
--- resources/views/resources/create.blade.php
+++ resources/views/resources/create.blade.php
@@ -3,10 +3,29 @@
 @section('title', trans('texts.upload_resource'))
 
 @section('content')
-  <article class="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 bg-white shadow radius padded margined">
-    <form class="form" action="{{ route('resources.store') }}" method="POST" enctype="multipart/form-data">
+  <article class="col-md-10 col-md-offset-1 bg-white shadow radius padded margined">
+    <form class="form validate" action="{{ route('resources.store') }}" method="POST" enctype="multipart/form-data">
       {!! csrf_field() !!}
       @include('admin.resources._form')
     </form>
   </article>
 @endsection
+
+@push('scripts')
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.3.1/js/fileinput.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-fileinput/4.3.1/js/fileinput_locale_es.min.js"></script>
+  <script src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/jquery.validate.min.js"></script>
+  <script src="http://cdn.jsdelivr.net/jquery.validation/1.15.0/additional-methods.min.js"></script>
+  <script type="text/javascript">
+    $('[type="file"]').fileinput({
+      showUpload: false,
+      overwriteInitial: false,
+      maxFileSize: 10000,
+      minFileCount: 1,
+      maxFileCount: 1,
+      language: "{{ App::getLocale() }}"
+    }).on('fileerror', function() {
+      $('button[type="submit"]').button('reset');
+    });
+  </script>
+@endpush
